8a8c279de44d3a0f78e69af5715677f3498fa0f0
Natalia0727 <oicafn@gmail.com>
2018-06-07 15:56:43 +0000

    Done.

---
+++ hashes.rb
@@ -0,0 +1,32 @@
+# hash = {"cheese" => "the stuff on pizza"}
+
+
+# character_names =["Monica","Joey","Pheobe"]
+# characteristics=["Neat Freak", 
+# "Jokester","Free Spirit"]
+
+# friends_characters = {}
+# index = 0
+# character_names.each do |name|
+#   friends_characters[name] = characteristics[index]
+#   index += 1
+# end
+# puts friends_characters
+
+# celeb_crush={} 
+# celeb_crush["emi"] = "grants gustin"
+# puts celeb_crush 
+# class_celeb_crushes = {
+#   "Natalia"=>"Timothee Chalemet",
+#   "Maya"=>"Michael B. Jordan",
+#   "Amira"=> "Cole Sprouse",
+# }
+# # puts class_celeb_crushes
+# puts class_celeb_crushes.values
+# puts class_celeb_crushes.keys
+
+# class_celeb_crushes.each do |names,crushes|
+#   puts "#{names}'s' celebrity crush is #{crushes}"
+  
+# end
+puts "3".to_f + 3.0
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,17 +1,43 @@
 
 def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+  summer_olymipics= {
+    :Sydney => 2000,
+    :Athens => 2004,
+    :Beijing => 2008, 
+    :London => 2012
+    
+  }
 end
-
+puts create_olympics_hash
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  summer_olymipics= {
+    :Sydney => 2000,
+    :Athens => 2004,
+    :Beijing => 2008, 
+    :London => 2012
+    
+  }
+  summer_olymipics[:Atlanta] = 1996
+  puts summer_olymipics
 end
-
+puts add_a_key_value_pair
 def iterate_through_hash
+  summer_olymipics= {
+    :Sydney => 2000,
+    :Athens => 2004,
+    :Beijing => 2008, 
+    :London => 2012
+    
+  }
+  summer_olymipics.each do |place,year|
+    puts "The #{place} summer olymipics took place in #{year}"
+    
+  end 
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
 end
-
+puts iterate_through_hash
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
